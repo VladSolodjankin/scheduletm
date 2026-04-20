3b48052da77afbd63cd4497fc39dabd40819111c
VladSolodjankin <vlad.s-92@yandex.ru>
2026-04-20 14:56:38 +0400

    refactor: move db migrations from bot to server

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -7,15 +7,22 @@ const config: Record<string, Knex.Config> = {
   development: {
     client: 'pg',
     connection: process.env.DATABASE_PUBLIC_URL,
+    migrations: {
+      directory: './src/db/migrations',
+      extension: 'ts',
+    },
     pool: {
       min: 0,
       max: 7,
     },
   },
-
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './dist/db/migrations',
+      extension: 'js',
+    },
     pool: {
       min: 0,
       max: 7,
