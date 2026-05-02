9ac94eb3b3cff77a6374467b5de472bf82f0e0a5
Vladislav Solodiankin <vladislav@maintstar.com>
2026-05-02 18:32:22 +0400

    .

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vitest/config';
+import { loadEnv } from 'vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   test: {
     environment: 'node',
     include: ['tests/**/*.test.ts'],
     clearMocks: true,
+    env: loadEnv(mode, process.cwd(), '')
   },
-});
+}))
